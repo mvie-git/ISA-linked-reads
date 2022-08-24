f4cb07dee69bc4fa45d885fc47cf988f52b7496e
Mallaury Vie <mallaury.vie@gmail.com>
2022-08-24 09:57:49 +0200

    Add script to extract barcode corresponding to mouse mapped reads

---
+++ scripts/MOLECULE-first/8.retrieve_barcodes_from_readID/retrieve_barcodes_from_readID_mouse.sh
@@ -0,0 +1,21 @@
+# Search through the whole index to retrieve the barcodes assigned to each read ID of interest
+
+workdir="/media/mallaury/MALLAURY1/PROJECT/GSDIa_ISA/hybrid_genome_mapping/add_barcode_to_BAM"
+
+## FOR EACH SAMPLE
+while read sample; do
+
+	echo "*** Start $sample ***"
+
+	# Create an output directory
+	mkdir -p ${sample}
+
+	# Input files
+	index_file=${workdir}/0.prepare_index_file/${sample}/${sample}_index.txt
+	readID_list=${workdir}/7.select_readID_of_interest/${sample}/${sample}_readID_list.txt
+
+	# Subset the index file keeping only barcodes assigned to the list of read ID of interest
+	#LC_ALL=C grep -w -f $readID_list $index_file > ${workdir}/8.retrieve_barcodes_from_readID/${sample}/${sample}_subset_index_file.txt
+	grep -w -f $readID_list $index_file > ${workdir}/8.retrieve_barcodes_from_readID/${sample}/${sample}_subset_index_file.txt
+
+done < sample_list_8.txt
